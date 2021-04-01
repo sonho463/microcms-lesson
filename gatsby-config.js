7676ebeb85527db585734cf9322e8daafdb3091c
sonho463 <son.ho463@gmail.com>
2021-04-01 19:38:26 +0900

    First intiial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,22 @@
+//gatsby-config.js
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     title: "microcms-lesson",
   },
-  plugins: [],
+  plugins: [
+		{
+      resolve: 'gatsby-source-microcms',
+      options: {
+        apiKey: process.env.MICROCMS_API_KEY,
+        serviceId: process.env.MICROCMS_SERVICE_ID,
+        apis: [{
+          endpoint: 'posts',
+        }],
+      },
+    },
+	],
 };
